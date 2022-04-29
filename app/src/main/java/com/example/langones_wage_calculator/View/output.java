da36ca372c0ee2e6207e0d5ead91049c548582ab
Miguel <toby.langones@gmail.com>
2022-04-29 17:30:00 +0800

    update 1

---
+++ app/src/main/java/com/example/langones_wage_calculator/View/output.java
@@ -0,0 +1,33 @@
+package com.example.langones_wage_calculator.View;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.langones_wage_calculator.R;
+
+import java.util.Objects;
+
+public class output extends AppCompatActivity {
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.output);
+        //fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //
+        //code to hide the action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        //
+    }
+}
